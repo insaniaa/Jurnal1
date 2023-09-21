52deab17d01551ab0c1e6bf452aa184c91119d9d
unknown <tiyasinsania@gmail.com>
2023-09-21 13:10:04 +0700

    Jurna 1

---
+++ Jurnal1.java
@@ -0,0 +1,7 @@
+public class Jurnal1 {
+    public static void main(String[] args) {
+        System.out.println("Nama: Tiyas Insania Dhikananda");
+        System.out.println("NIM: 607062300090");
+        System.out.println("Kelas: D3IF-47-03");
+    }
+}
